3b69c022c68e565a25aa9192bf6801ba99420338
kberezuck <kseniaberezuck@gmail.com>
2023-07-29 23:23:43 +0300

    создала dm_api_account

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # REQUIRES = [
 #     'requests',
@@ -10,7 +10,7 @@ from setuptools import setup
 setup(
     name='dm_api_account',
     version='0.0.2',
-    packages=['dm_api_account', 'models', 'apis'],
+    packages=find_packages(),
     url='https://github.com/kberezuck/dm_api_account.git',
     license='MIT',
     author='Ksenia_Berezuck',
