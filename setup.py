31e4ce7081ac60c1be54ddb596c141905fb9b7e5
kberezuck <kseniaberezuck@gmail.com>
2023-07-29 16:42:02 +0300

    создала dm_api_account

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+REQUIRES = [
+    'requests',
+    'restclient',
+    'pydantic',
+    'structlog',
+    'allure-pytest',
+
+]
+setup(
+    name='dm_api_account',
+    version='0.0.1',
+    packages=['dm_api_account'],
+    url='https://github.com/kberezuck/dm_api_account.git',
+    license='MIT',
+    author='Ksenia_Berezuck',
+    author_email='',
+    install_requires=REQUIRES,
+    description='dm_api_account with allure and login'
+)
