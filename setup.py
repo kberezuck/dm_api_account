b41675b367e2902069c6cdfc6ef0f1a10d6dc798
kberezuck <kseniaberezuck@gmail.com>
2023-07-29 23:37:06 +0300

    создала dm_api_account2

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
-# REQUIRES = [
-#     'requests',
-#     'pydantic',
-#     'structlog',
-#     'allure-pytest',
-#
-# ]
+REQUIRES = [
+    'requests',
+    'pydantic',
+    'structlog',
+    'allure-pytest',
+
+]
 setup(
     name='dm_api_account',
-    version='0.0.2',
-    packages=find_packages('dm_api_account.apis', 'dm_api_account.models'),
+    version='0.0.3',
+    packages=find_packages(),
     url='https://github.com/kberezuck/dm_api_account.git',
     license='MIT',
     author='Ksenia_Berezuck',
     author_email='',
-    # install_requires=REQUIRES,
+    install_requires=REQUIRES,
     description='dm_api_account with allure and login'
 )
